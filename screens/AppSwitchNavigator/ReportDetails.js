34095954fa0d82c7367cab83a7225a198a4bb93d
John Biddulph <john.mbiddulph@gmail.com>
2020-06-05 21:53:14 +0100

    added images and some styling to details screen

--- screens/AppSwitchNavigator/ReportDetails.js
+++ screens/AppSwitchNavigator/ReportDetails.js
@@ -1,34 +1,46 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {View, Text, StyleSheet, Dimensions, Image} from 'react-native';
 import {globalStyles} from "../../styles/global";
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import colors from "../../assets/colors";
 
 class ReportDetails extends Component {
 
     constructor(props) {
         super(props);
+        console.log(props)
     }
     render() {
         const navigation = this.props.route.params
         return (
 
             <View style={styles.container}>
-                <MapView
-                    provider={PROVIDER_GOOGLE}
-                    style={styles.mapStyle}
-                    initialRegion={{
-                        latitude: this.props.route.params.latitude,
-                        longitude: this.props.route.params.longitude
-                    }} />
-                    {/*<Marker coordinate={marker.latlng}>*/}
-                    {/*    <MyCustomMarkerView {...marker} />*/}
-                    {/*    <Callout>*/}
-                    {/*        <MyCustomCalloutView {...marker} />*/}
-                    {/*    </Callout>*/}
-                    {/*</Marker>*/}
-                <View style={styles.header}>
-                    <Text>{this.props.route.params.name}</Text>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                    <MapView
+                        provider={PROVIDER_GOOGLE}
+                        style={styles.mapStyle}
+                        showsUserLocation={true}
+                        initialRegion={{
+                            latitude: this.props.route.params.latitude,
+                            longitude: this.props.route.params.longitude,
+                            latitudeDelta: 0.02,
+                            longitudeDelta: 0.02
+                        }}>
+                        <Marker
+                            coordinate={{
+                                latitude: this.props.route.params.latitude,
+                                longitude: this.props.route.params.longitude
+                            }}
+                            title={this.props.route.params.name}
+                            description="hello there"
+                        />
+                    </MapView>
+                    <View style={styles.header}>
+                        <Image source={{uri: this.props.route.params.image }} style={styles.image}/>
+                    </View>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                    <Text  style={styles.headertitle}>{this.props.route.params.name}</Text>
                 </View>
             </View>
 
@@ -40,13 +52,28 @@ export default ReportDetails;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: colors.bgMain,
+        alignItems: 'center'
     },
     header: {
         fontSize: 30,
+        flex: 1,
+        height: 340
+    },
+    headertitle: {
+        fontSize: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: colors.yellow
     },
     mapStyle: {
-        width: Dimensions.get('window').width,
-        height: 300,
+        flex: 1,
+        height: 340,
+    },
+    image: {
+        flex: 1,
+        height: 340,
+        flexDirection: 'column'
     }
 })
